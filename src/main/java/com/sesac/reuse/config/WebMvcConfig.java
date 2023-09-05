e42f54cf9bc25605ebf1e7bd08467ea71c39161f
dahee oh <ekgml4122@gmail.com>
2023-09-06 03:05:17 +0900

    예진님과 merge 후 Search 경로 수정

--- src/main/java/com/sesac/reuse/config/WebMvcConfig.java
+++ src/main/java/com/sesac/reuse/config/WebMvcConfig.java
@@ -1,10 +1,20 @@
 package com.sesac.reuse.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index"); //setViewName : viewName 이라서 "/index" 이렇게 경로 ㄴㄴ
+        registry.addViewController("/home").setViewName("index");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
@@ -12,3 +22,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/"); // 클래스패스 내의 정적 리소스 디렉토리를 지정
     }
 }
+
---
+++ src/test/java/com/sesac/reuse/security/service/CustomMemberDetailsServiceTest.java
@@ -0,0 +1,15 @@
+package com.sesac.reuse.security.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+class CustomMemberDetailsServiceTest {
+
+    @Autowired
+    private CustomMemberDetailsService memberDetailsService;
+
+
+}
